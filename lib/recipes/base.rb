0c67e0f2f5aa1389ef93d5736f396208c12bd49b
Kacper Kawecki <kacper@subsis.com>
2012-05-10 15:35:05 +0200

    updated base install

--- lib/recipes/base.rb
+++ lib/recipes/base.rb
@@ -12,7 +12,7 @@ Capistrano::Configuration.instance.load do
     desc "Install everything on server"
     task :install do
       run "#{sudo} apt-get -y update"
-      run "#{sudo} apt-get -y install python-software-properties"
+      run "#{sudo} apt-get -y install python-software-properties curl build-essential git-core"
     end
   end
 end
