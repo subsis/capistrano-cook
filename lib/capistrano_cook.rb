57abf0bc9eac15ec488592d36cde7f8e8f4c5e9a
Kacper Kawecki <kacper@subsis.com>
2012-05-10 16:29:17 +0200

    Added loading paths

--- lib/capistrano_cook.rb
+++ lib/capistrano_cook.rb
@@ -3,3 +3,9 @@ require 'capistrano/cli'
 require 'helpers'
 
 Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
+load "/recipes/server/nginx"
+load "/recipes/server/unicorn"
+load "/recipes/db/mysql"
+load "/recipes/ruby/nodejs"
+load "/recipes/ruby/rbenv"
+load "assets"
