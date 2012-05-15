69984176ffec523afbd242b496bd84823ae8394f
Kacper Kawecki <kacper@subsis.com>
2012-05-15 14:07:59 +0200

    added gems dependences recipe

--- lib/capistrano-cook.rb
+++ lib/capistrano-cook.rb
@@ -8,4 +8,5 @@ load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/server/unicorn.rb')).f
 load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/db/mysql.rb')).first
 load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/ruby/nodejs.rb')).first
 load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/ruby/rbenv.rb')).first
+load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/ruby/gems_dependences.rb')).first
 load Dir.glob(File.join(File.dirname(__FILE__), '/assets.rb')).first
---
+++ lib/recipes/ruby/gems_dependences.rb
@@ -0,0 +1,10 @@
+Capistrano::Configuration.instance.load do
+  namespace :gem_dependences do
+    desc "lib v8 libraries"
+    task :libv8, role: :app do
+      run "#{sudo} apt-get -y update"
+      run "#{sudo} apt-get -y install libv8-3.7.12.22 libv8-dev"
+    end
+    befor "bundle:install", "gem_dependences:libv8"
+  end
+end
