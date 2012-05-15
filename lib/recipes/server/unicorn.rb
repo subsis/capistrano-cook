ed6279ee22d79ff27ddf140f9188a5f9bf8810e4
Kacper Kawecki <kacper@subsis.com>
2012-05-15 11:39:34 +0200

    repaired unicorn setup.

--- lib/recipes/server/unicorn.rb
+++ lib/recipes/server/unicorn.rb
@@ -1,5 +1,7 @@
 Capistrano::Configuration.instance.load do
   set_default(:unicorn_user) { user }
+  set_default(:unicorn_template, "unicorn.rb.erb")
+  set_default(:unicorn_init_template, "unicorn_init.erb" )
   set_default(:unicorn_pid) { "#{current_path}/tmp/pids/unicorn.pid" }
   set_default(:unicorn_config) { "#{shared_path}/config/unicorn.rb" }
   set_default(:unicorn_log) { "#{shared_path}/log/unicorn.log" }
@@ -8,9 +10,9 @@ Capistrano::Configuration.instance.load do
   namespace :unicorn do
     desc "Setup Unicorn initializer and app configuration"
     task :setup, roles: :app do
-      run "mkdir -p #{shared_path}/config"
-      template "unicorn.rb.erb", unicorn_config
-      template "unicorn_init.erb", "/tmp/unicorn_init"
+      run "#{sudo} mkdir -p #{shared_path}/config"
+      template unicorn_template, unicorn_config
+      template unicorn_init_template, "/tmp/unicorn_init"
       run "chmod +x /tmp/unicorn_init"
       run "#{sudo} mv /tmp/unicorn_init /etc/init.d/unicorn_#{application}"
       run "#{sudo} update-rc.d -f unicorn_#{application} defaults"
