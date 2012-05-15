4f81becf374349056246a82a607ada8e7fc86052
Kacper Kawecki <kacper@subsis.com>
2012-05-15 12:29:02 +0200

    repaired mysql

--- lib/recipes/db/mysql.rb
+++ lib/recipes/db/mysql.rb
@@ -18,9 +18,9 @@ Capistrano::Configuration.instance.load do
 
     desc "Create user and database for the application"
     task :create_database, roles: :db do
-      run %Q{mysql -uroot -p="#{db_root_password}" -e "create user '#{db_user}'@'#{db_host}' identyfied by #{db_password};"}
-      run %Q{mysql -uroot -p="#{db_root_password}" -e "create databse #{db_name};"}
-      run %Q{mysql -uroot -p="#{db_root_password}" -e "GRANT ALL PRIVILEGES ON *.* TO '#{db_user}'@'#{db_host}'"}
+      run %Q{mysql -p=#{db_root_password} -e "create user '#{db_user}'@'#{db_host}' identyfied by '#{db_password}';"}
+      run %Q{mysql -p=#{db_root_password} -e "create databse #{db_name};"}
+      run %Q{mysql -p=#{db_root_password} -e "GRANT ALL PRIVILEGES ON *.* TO '#{db_user}'@'#{db_host}'"}
     end
     after "deploy:setup", "mysql:create_database"
 
