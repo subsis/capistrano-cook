0d1bcfd5881c354e43b3eaab679b3156426dd991
Kacper Kawecki <kacper@subsis.com>
2012-05-15 10:50:14 +0200

    Added -y to add-apt repository command

--- lib/recipes/ruby/nodejs.rb
+++ lib/recipes/ruby/nodejs.rb
@@ -2,7 +2,7 @@ Capistrano::Configuration.instance.load do
   namespace :nodejs do
     desc "Install the latest relase of Node.js"
     task :install, roles: :app do
-      run "#{sudo} add-apt-repository ppa:chris-lea/node.js"
+      run "#{sudo} add-apt-repository -y ppa:chris-lea/node.js"
       run "#{sudo} apt-get -y update"
       run "#{sudo} apt-get -y install nodejs"
     end
