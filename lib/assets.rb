8bcfe667d5f3338e53f54e62f25f549ed8381138
Kacper Kawecki <kacper@subsis.com>
2012-05-15 09:36:50 +0200

    Removed asset bug

--- lib/assets.rb
+++ lib/assets.rb
@@ -8,3 +8,4 @@ if exists?(:assets) && assets == :local
   load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/assets/local_precompile.rb')).first
 else
   load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/assets/lazy_precompile.rb')).first
+end
