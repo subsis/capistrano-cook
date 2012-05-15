c544d6f9fbd58061e7fed3946ca1fb53a30f76b1
Kacper Kawecki <kacper@subsis.com>
2012-05-15 09:35:03 +0200

    updated path to recipes.

--- lib/assets.rb
+++ lib/assets.rb
@@ -5,6 +5,6 @@ require 'helpers'
 Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
 
 if exists?(:assets) && assets == :local
-  load '/recipes/assets/local_precompile'
+  load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/assets/local_precompile.rb')).first
 else
-  load '/recipes/assets/lazy_precompile'
+  load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/assets/lazy_precompile.rb')).first
--- lib/capistrano-cook.rb
+++ lib/capistrano-cook.rb
@@ -3,9 +3,9 @@ require 'capistrano/cli'
 require 'helpers'
 
 Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
-load "/recipes/server/nginx"
-load "/recipes/server/unicorn"
-load "/recipes/db/mysql"
-load "/recipes/ruby/nodejs"
-load "/recipes/ruby/rbenv"
-load "assets"
+load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/server/nginx.rb')).first
+load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/server/unicorn.rb')).first
+load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/db/mysql.rb')).first
+load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/ruby/nodejs.rb')).first
+load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/ruby/rbenv.rb')).first
+load Dir.glob(File.join(File.dirname(__FILE__), '/assets.rb')).first
