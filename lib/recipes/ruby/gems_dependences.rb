cd37c27c8fa79d2fa5971ab4e9502f4490204112
Kacper Kawecki <kacper@subsis.com>
2012-05-15 14:23:43 +0200

    libv8 fix

--- lib/recipes/ruby/gems_dependences.rb
+++ lib/recipes/ruby/gems_dependences.rb
@@ -1,10 +1,12 @@
 Capistrano::Configuration.instance.load do
+  set_default :libv8_version, "3.3.10.2"
+
   namespace :gem_dependences do
     desc "lib v8 libraries"
     task :libv8, role: :app do
       run "#{sudo} apt-get -y update"
       run "#{sudo} apt-get -y install libv8-3.7.12.22 libv8-dev"
-      run "gem install --version '=3.3.10.4' libv8"
+      run "gem install --version '=#{libv8_version}' libv8"
     end
     before "bundle:install", "gem_dependences:libv8"
   end
