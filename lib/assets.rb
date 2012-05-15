6f97cc7b229c5f9637565c5d71b39cc9904042a9
Kacper Kawecki <kacper@subsis.com>
2012-05-15 09:47:29 +0200

    removed asset bug

--- lib/assets.rb
+++ lib/assets.rb
@@ -4,7 +4,7 @@ require 'helpers'
 
 Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
 
-if exists?(:assets) && assets == :local
+if Capistrano::Configuration::Variables.exists?(:assets) && assets == :local
   load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/assets/local_precompile.rb')).first
 else
   load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/assets/lazy_precompile.rb')).first
