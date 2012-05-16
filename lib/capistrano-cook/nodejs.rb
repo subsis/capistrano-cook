f6212a848ad4682e96b030d41f0ebe89d5f061ac
Kacper Kawecki <kacper@subsis.com>
2012-05-16 11:00:01 +0200

    nodejs loading script

---
+++ lib/capistrano-cook/nodejs.rb
@@ -0,0 +1,10 @@
+require 'capistrano'
+require 'capistrano/cli'
+require 'capistrano-cook/helpers'
+
+Capistrano::Configuration.instance.load do
+  set :nodejs, :true
+end
+
+Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
+load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/ruby/nodejs.rb')).first
