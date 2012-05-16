491a09518984a45a54d9d20aad69168ef4ef56f6
Kacper Kawecki <kacper@subsis.com>
2012-05-16 09:59:29 +0200

    Changed helpers path

--- lib/capistrano-cook/assets.rb
+++ lib/capistrano-cook/assets.rb
@@ -1,6 +1,6 @@
 require 'capistrano'
 require 'capistrano/cli'
-require 'helpers'
+require 'capistrano-cook/helpers'
 
 Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
 Dir.glob(File.join(File.dirname(__FILE__), '/recipes/assets/*.rb')).sort.each { |f| load f }
