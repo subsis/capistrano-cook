00ab1c213e5268fc708f3bc583755d5985dd57d6
Kacper Kawecki <kacper@subsis.com>
2012-05-16 11:00:12 +0200

    rbenv loading script

---
+++ lib/capistrano-cook/rbenv.rb
@@ -0,0 +1,10 @@
+require 'capistrano'
+require 'capistrano/cli'
+require 'capistrano-cook/helpers'
+
+Capistrano::Configuration.instance.load do
+  set :ruby_installer, :rbenv
+end
+
+Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
+load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/ruby/rbenv.rb')).first
