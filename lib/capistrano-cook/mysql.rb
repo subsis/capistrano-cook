ca9594d025e653214a44f0a936083c838c865710
Kacper Kawecki <kacper@subsis.com>
2012-05-16 10:23:09 +0200

    added loading mysql

---
+++ lib/capistrano-cook/mysql.rb
@@ -0,0 +1,10 @@
+require 'capistrano'
+require 'capistrano/cli'
+require 'capistrano-cook/helpers'
+
+Capistrano::Configuration.instance.load do
+  set :db_server, :mysql
+end
+
+Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
+load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/db/mysql.rb')).first
