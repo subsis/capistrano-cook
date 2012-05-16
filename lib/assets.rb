d25708d02acc8220fe4c940f5fa2ef9c1e370332
Kacper Kawecki <kacper@subsis.com>
2012-05-16 09:37:24 +0200

    changed asset loading

--- lib/assets.rb
+++ lib/assets.rb
@@ -2,10 +2,5 @@ require 'capistrano'
 require 'capistrano/cli'
 require 'helpers'
 
-#Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
+Dir.glob(File.join(File.dirname(__FILE__), '/recipes/assets/*.rb')).sort.each { |f| load f }
 
-#if Capistrano::Configuration::Variables.exists?(:assets) && assets == :local
-  load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/assets/local_precompile.rb')).first
-#else
-#  load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/assets/lazy_precompile.rb')).first
-#end
