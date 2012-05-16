13e1e178156c0752e5b16680a7eed7d77327d38d
Kacper Kawecki <kacper@subsis.com>
2012-05-16 10:08:29 +0200

    Added task descriptions for assets

--- lib/capistrano-cook/recipes/assets/lazy_precompile.rb
+++ lib/capistrano-cook/recipes/assets/lazy_precompile.rb
@@ -1,6 +1,7 @@
 Capistrano::Configuration.instance.load do
   namespace :deploy do
     namespace :assets do
+      desc "precompiles assets only when change occures"
       task :lazy_precompile, :roles => :web, :except => { :no_release => true } do
         if capture("cd #{latest_release} && #{source.local.log(current_revision)} app/assets/ lib/assets/ vendor/assets/ | wc -l").to_i > 0
           run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} assets:precompile}
