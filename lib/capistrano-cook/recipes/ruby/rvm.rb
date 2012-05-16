401aa9d4c931fcad642e97a943a68afae5f77cd6
Kacper Kawecki <kacper@subsis.com>
2012-05-16 16:16:31 +0200

    rvm

--- lib/capistrano-cook/recipes/ruby/rvm.rb
+++ lib/capistrano-cook/recipes/ruby/rvm.rb
@@ -36,7 +36,7 @@ Capistrano::Configuration.instance(true).load do
       command_install << "#{rvm_install_shell} -s stable --path #{rvm_path}"
 
       run "#{command_fetch} #{command_install}", :shell => "#{rvm_install_shell}"
-      run "export $PATH=\"#{rvm_path}/bin:$PATH\""
+      run "export $PATH='#{rvm_path}/bin:$PATH'"
       run "rvm install #{ruby_version} -j #{rvm_install_ruby_threads} #{rvm_install_ruby_params}", :shell => "#{rvm_install_shell}"
       run "rvm install #{ruby_version} -j #{rvm_install_ruby_threads} #{rvm_install_ruby_params}", :shell => "#{rvm_install_shell}"
       run "rvm #{ruby_version} do rvm gemset create #{rvm_gemset}", :shell => "#{rvm_install_shell}"
