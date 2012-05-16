a90749dcc434c5879a7b6ee0b2079b57b3de6899
Kacper Kawecki <kacper@subsis.com>
2012-05-16 10:16:21 +0200

    changed file loading

--- lib/capistrano-cook.rb
+++ lib/capistrano-cook.rb
@@ -3,7 +3,7 @@ require 'capistrano/cli'
 require 'capistrano-cook/helpers'
 
 Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/*.rb'       )).sort.each { |f| load f }
-Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/assets/*.rb')).sort.each { |f| load f }
+Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/server/*.rb')).sort.each { |f| load f }
 Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/db/*.rb'    )).sort.each { |f| load f }
 Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/ruby/*.rb'  )).sort.each { |f| load f }
-Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/server/*.rb')).sort.each { |f| load f }
+Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/assets/*.rb')).sort.each { |f| load f }
--- lib/capistrano-cook/recipes/assets/precompile.rb
+++ lib/capistrano-cook/recipes/assets/precompile.rb
@@ -1,17 +1,9 @@
 Capistrano::Configuration.instance.load do
   namespace :deploy do
     namespace :assets do
-      set_default :assets, :lazy
-
       desc "precompile assets"
       task :precompile, :roles => :web, :except => { :no_release => true } do
-        if assets == :local
-           local_precompile
-        elsif assets == :lazy
-           lazy_precompile
-        else
           run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} assets:precompile}
-        end
       end
     end
   end
