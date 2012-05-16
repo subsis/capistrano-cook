a0394b53cf349988465bd2f8b008d147ab35e5dc
Kacper Kawecki <kacper@subsis.com>
2012-05-16 10:44:20 +0200

    repaired paths

--- lib/capistrano-cook/fix-gems.rb
+++ lib/capistrano-cook/fix-gems.rb
@@ -3,4 +3,4 @@ require 'capistrano/cli'
 require 'capistrano-cook/helpers'
 
 Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
-load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/ruby/gem_dependences.rb')).first
+load Dir.glob(File.join(File.dirname(__FILE__), '/recipes/ruby/gems_dependences.rb')).first
