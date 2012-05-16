273c5cac2fc055dcd506fd7ec9e17f0ba3d60028
Kacper Kawecki <kacper@subsis.com>
2012-05-16 09:53:31 +0200

    changed folder structure

--- lib/capistrano-cook.rb
+++ lib/capistrano-cook.rb
@@ -1,9 +1,9 @@
 require 'capistrano'
 require 'capistrano/cli'
-require 'helpers'
+require 'capistrano-cook/helpers'
 
-Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb'       )).sort.each { |f| load f }
-Dir.glob(File.join(File.dirname(__FILE__), '/recipes/assets/*.rb')).sort.each { |f| load f }
-Dir.glob(File.join(File.dirname(__FILE__), '/recipes/db/*.rb'    )).sort.each { |f| load f }
-Dir.glob(File.join(File.dirname(__FILE__), '/recipes/ruby/*.rb'  )).sort.each { |f| load f }
-Dir.glob(File.join(File.dirname(__FILE__), '/recipes/server/*.rb')).sort.each { |f| load f }
+Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/*.rb'       )).sort.each { |f| load f }
+Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/assets/*.rb')).sort.each { |f| load f }
+Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/db/*.rb'    )).sort.each { |f| load f }
+Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/ruby/*.rb'  )).sort.each { |f| load f }
+Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/server/*.rb')).sort.each { |f| load f }
--- lib/capistrano-cook/assets.rb
+++ lib/capistrano-cook/assets.rb
@@ -2,5 +2,5 @@ require 'capistrano'
 require 'capistrano/cli'
 require 'helpers'
 
+Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
 Dir.glob(File.join(File.dirname(__FILE__), '/recipes/assets/*.rb')).sort.each { |f| load f }
-
