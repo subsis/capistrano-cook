1b0e5d68d62c04a76515708b268ef1e72c756c2e
andreas <andreas@subsis.com>
2012-05-24 15:23:15 +0200

    Update Unicorn config on deploy:update_code.

--- lib/capistrano-cook/recipes/server/unicorn.rb
+++ lib/capistrano-cook/recipes/server/unicorn.rb
@@ -9,11 +9,17 @@ Capistrano::Configuration.instance.load do
   set_default(:rails_server, :unicorn)
 
   namespace :unicorn do
-    desc "Setup Unicorn initializer and app configuration"
-    task :setup, roles: :app do
+    desc "Update Unicorn app configuration"
+    task :update_config, roles: :app do
       run "#{sudo} mkdir -p #{shared_path}/config"
       template unicorn_template, "/tmp/unicorn.rb"
       run "#{sudo} mv /tmp/unicorn.rb #{unicorn_config}"
+    end
+
+    desc "Setup Unicorn initializer and app configuration"
+    task :setup, roles: :app do
+      update_config
+
       template unicorn_init_template, "/tmp/unicorn_init"
       run "chmod +x /tmp/unicorn_init"
       run "#{sudo} mv /tmp/unicorn_init /etc/init.d/unicorn_#{application}"
@@ -21,6 +27,10 @@ Capistrano::Configuration.instance.load do
     end
     after "deploy:setup" do
       setup if rails_server == :unicorn
+    end
+
+    after "deploy:update_code" do
+      update_config if rails_server == :unicorn
     end
 
     %w[start stop restart].each do |command|
