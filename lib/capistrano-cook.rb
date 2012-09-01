509b5268fed9eae308e270ea3867196e98f5d746
Mark <mark.pietrucha@gmail.com>
2012-09-01 22:25:27 +0200

    bug fix: trims the .rb ext in __FILE__ variable when constructing a load path to dynamically load other ruby files in capistrano-cook.rb file

--- lib/capistrano-cook.rb
+++ lib/capistrano-cook.rb
@@ -2,8 +2,8 @@ require 'capistrano'
 require 'capistrano/cli'
 require 'capistrano-cook/helpers'
 
-Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/*.rb'       )).sort.each { |f| load f }
-Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/server/*.rb')).sort.each { |f| load f }
-Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/db/*.rb'    )).sort.each { |f| load f }
-Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/ruby/*.rb'  )).sort.each { |f| load f }
-Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-cook/recipes/assets/*.rb')).sort.each { |f| load f }
+Dir.glob(File.join(File.dirname(__FILE__)[0...-3], '/capistrano-cook/recipes/*.rb'       )).sort.each { |f| load f }
+Dir.glob(File.join(File.dirname(__FILE__)[0...-3], '/capistrano-cook/recipes/server/*.rb')).sort.each { |f| load f }
+Dir.glob(File.join(File.dirname(__FILE__)[0...-3], '/capistrano-cook/recipes/db/*.rb'    )).sort.each { |f| load f }
+Dir.glob(File.join(File.dirname(__FILE__)[0...-3], '/capistrano-cook/recipes/ruby/*.rb'  )).sort.each { |f| load f }
+Dir.glob(File.join(File.dirname(__FILE__)[0...-3], '/capistrano-cook/recipes/assets/*.rb')).sort.each { |f| load f }
