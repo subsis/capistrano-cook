05b6a44506e43554ddbbad79a5169e9b24da0a1b
Mark <mark.pietrucha@gmail.com>
2012-09-02 02:17:47 +0200

    change __FILE__ to __DIR__

--- lib/capistrano-cook/assets.rb
+++ lib/capistrano-cook/assets.rb
@@ -2,5 +2,5 @@ require 'capistrano'
 require 'capistrano/cli'
 require 'capistrano-cook/helpers'
 
-Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
-Dir.glob(File.join(File.dirname(__FILE__), '/recipes/assets/*.rb')).sort.each { |f| load f }
+Dir.glob(File.join(File.dirname(__DIR__), '/recipes/*.rb')).sort.each { |f| load f }
+Dir.glob(File.join(File.dirname(__DIR__), '/recipes/assets/*.rb')).sort.each { |f| load f }
