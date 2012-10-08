d646f06f93f30cdea37d46bf7a3de01cf55a6039
andreas <andreas@subsis.com>
2012-10-08 12:06:05 +0200

    Memcached recipe.

---
+++ lib/capistrano-cook/recipes/server/memcached.rb
@@ -0,0 +1,12 @@
+Capistrano::Configuration.instance.load do
+  namespace :memcached do
+    desc "Install Memcached server"
+    task :install, :roles => :web do
+      run "#{sudo} apt-get -y install memcached"
+    end
+
+    after "deploy:install" do
+      install
+    end
+  end
+end
