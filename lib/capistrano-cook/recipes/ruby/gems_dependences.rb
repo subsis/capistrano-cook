ab9d85ac176d4e3b545ace697accf6892d8cf63e
Nicholas Schultz-Møller <nicholassm@gmail.com>
2012-11-01 17:55:29 +0100

    Automatically infer which additional libs to install.
    Now looks at Gemfile.lock to see which gems are used
    and installs necessary linux libraries.

--- lib/capistrano-cook/recipes/ruby/gems_dependences.rb
+++ lib/capistrano-cook/recipes/ruby/gems_dependences.rb
@@ -1,35 +1,44 @@
 Capistrano::Configuration.instance.load do
-  set_default :libv8_version, "3.3.10.2"
+  set_default :libv8_version,       "3.3.10.2"
   set_default :system_architecture, "x86-linux"
 
   set_default :fix_libv8,    false
-  set_default :fix_nokogiri, false
-  set_default :fix_patron,   false
 
   namespace :gem_dependences do
+
+    before "bundle:install" do
+      libv8 if fix_libv8 == true
+    end
+
+    after "deploy:setup" do
+      gemfile_lock = File.read("Gemfile.lock")
+
+      %w{nokogiri patron dragonfly}.each do |gem|
+        send(:"#{gem}") if gemfile_lock =~ /#{gem}/
+      end
+    end
+
     desc "lib v8 libraries"
     task :libv8, :role => :app do
       run "cd #{release_path}/vendor/cache; wget 'http://rubygems.org/downloads/libv8-#{libv8_version}-#{system_architecture}.gem'"
     end
 
-    desc "nokogiri dependency library"
+    desc "installs nokogiri's library dependencies"
     task :nokogiri, :role => :app do
       run "#{sudo} apt-get -y update"
       run "#{sudo} apt-get -y install libxml2 libxml2-dev libxslt1-dev"
     end
 
-    desc "patron dependency library"
+    desc "installs patron's library dependencies"
     task :patron, :role => :app do
       run "#{sudo} apt-get -y update"
       run "#{sudo} apt-get -y install libcurl4-openssl-dev"
     end
 
-    before "bundle:install" do
-      libv8  if fix_libv8 == true
-    end
-    after "deploy:setup" do
-      nokogiri if fix_nokogiri == true
-      patron if fix_patron == true
+    desc "installs dragonfly's library dependencies"
+    task :dragonfly, :role => :app do
+      run "#{sudo} apt-get -y update"
+      run "#{sudo} apt-get -y install imagemagick"
     end
   end
 end
