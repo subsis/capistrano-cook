eb1bcc928558526d602dae9e3e7f1d8b27b0ff0b
Nicholas Schultz-Møller <nicholassm@gmail.com>
2012-11-06 11:38:14 +0100

    Set default Ruby version to 1.9.3-p286.

--- lib/capistrano-cook/recipes/ruby/rbenv.rb
+++ lib/capistrano-cook/recipes/ruby/rbenv.rb
@@ -1,5 +1,5 @@
 Capistrano::Configuration.instance.load do
-  set_default :ruby_version, "1.9.3-p125"
+  set_default :ruby_version, "1.9.3-p286"
   set_default :rbenv_bootstrap, "bootstrap-ubuntu-10-04"
   set_default :ruby_installer, :rbenv
 
@@ -20,13 +20,11 @@ BASHRC
       run "mv ~/.bashrc.tmp ~/.bashrc"
       run %q{export PATH="$HOME/.rbenv/bin:$PATH"}
       run %q{eval "$(rbenv init -)"}
-      #run "#{sudo} rbenv #{rbenv_bootstrap}"
       run "rbenv install #{ruby_version}"
       run "rbenv global #{ruby_version}"
       run "gem install bundler --no-ri --no-rdoc"
       run "rbenv rehash"
     end
-
 
     after "deploy:install" do
       install if ruby_installer == :rbenv
