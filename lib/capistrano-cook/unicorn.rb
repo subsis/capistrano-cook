190d57a489bb223e66ea65979ead980038440857
Kacper Kawecki <kacper@geniebelt.com>
2013-10-18 10:00:02 +0200

    Small fixes

--- lib/capistrano-cook/unicorn.rb
+++ lib/capistrano-cook/unicorn.rb
@@ -3,7 +3,7 @@ require 'capistrano/cli'
 require 'capistrano-cook/helpers'
 
 Capistrano::Configuration.instance.load do
-  set :rails_server, :unicorn
+  #set :rails_server, :unicorn
 end
 
 Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
