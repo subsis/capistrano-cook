341ef1c4b5e1255b27f115c289a4104944ecdec8
Nicholas Schultz-Møller <nicholassm@gmail.com>
2016-06-13 22:26:05 +0200

    Simplify nodejs recipe.

--- lib/capistrano-cook/recipes/ruby/nodejs.rb
+++ lib/capistrano-cook/recipes/ruby/nodejs.rb
@@ -3,8 +3,6 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :nodejs do
     desc "Install the latest relase of Node.js"
     task :install, :roles => :app do
-      run "#{sudo} add-apt-repository -y ppa:chris-lea/node.js"
-      run "#{sudo} apt-get -y update"
       run "#{sudo} apt-get -y install nodejs"
     end
     after "deploy:install" do
